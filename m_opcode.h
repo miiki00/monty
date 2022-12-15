1b9b810e5f727c137e759e85a17b70e1f070fdde
mikias <mikiasabiy16@gmail.com>
2022-12-14 19:48:50 -0500

    add: m_opcode*: declaration and defination of functinos related to operation codes of monty bytecode

---
+++ m_opcode.h
@@ -0,0 +1,24 @@
+#ifndef M_OPCODE_H
+#define M_OPCODE_H
+
+/* opcodes functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_nubmer);
+/* void div(stack_t **stack, unsigned int line_number); */
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rot1(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
+
+#endif
